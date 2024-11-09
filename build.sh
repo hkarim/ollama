f818b16e1a0dc0248ce92f75288fb22e0e5c5191
hkarim <hossam.karim@gmail.com>
2024-11-09 18:30:47 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 make -j 16 GGML_HIPBLAS=1 GGML_HIP_UMA=1 AMDGPU_TARGETS=gfx1100 CLBlast_DIR=/usr/lib/x86_64-linux-gnu/cmake/CLBlast ROCM_PATH=/opt/rocm 
 go build .
+
+# OLLAMA_HOST=0.0.0.0 LD_LIBRARY_PATH=$(pwd)/dist/linux-amd64/lib/ollama ./ollama serve
